542af54ba70994d49ecacf527e49fdebe92eeca8
Dev-Olena <alenagruda@gmail.com>
2025-01-28 09:33:43 +0200

    add plants data

---
+++ src/data/plants.js
@@ -0,0 +1,60 @@
+export const plants = [
+    {
+		name: 'monstera',
+        img: '/assets/plants/monstera.jpg',
+        price: 16.00,
+		id: '1ed'
+	},
+	{
+		name: 'ficus lyrata',
+		img: '/assets/plants/ficus-lyrata.jpg',
+        price: 17.00,
+		id: '2ab'
+	},
+	{
+		name: 'calathea',
+		img: '/assets/plants/calathea.jpg',
+        price: 15.99,
+		id: '3sd',
+		isSpecialOffer: true
+	},
+	{
+		name: 'yucca',
+		img: '/assets/plants/monstera.jpg',
+        price: 19.00,
+		id: '4kk'
+	},
+	{
+		name: 'olive tree',
+		img: '/assets/plants/monstera.jpg',
+        price: 18.00,
+		id: '5pl',
+		isAvailable: false
+	},
+	{
+		name: 'geranium',
+		img: '/assets/plants/monstera.jpg',
+        price: 15.00,
+		id: '6uo'
+	},
+	{
+		name: 'basil',
+		img: '/assets/plants/monstera.jpg',
+        price: 15.99,
+		id: '7ie',
+		isSpecialOffer: true
+	},
+	{
+		name: 'aloe',
+		img: '/assets/plants/monstera.jpg',
+        price: 12.99,
+		id: '8fp',
+		isSpecialOffer: true
+	},
+	{
+		name: 'succulent',
+		img: '/assets/plants/monstera.jpg',
+        price: 13.00,
+		id: '9vn'
+	}
+]
